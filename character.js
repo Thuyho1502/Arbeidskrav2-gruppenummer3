50140e5d7c2e1da45d506e5aa1e157cc371e3abe
Thuyho1502 <p2thanhthuy1502@gmail.com>
2025-03-23 15:09:37 +0100

    fix crateCharacter function

--- character.js
+++ character.js
@@ -1,234 +1,229 @@
-document.addEventListener("DOMContentLoaded",async() =>{
-    const allCharacters = document.getElementById("all_character");
-    const speciesFilter = document.getElementById("species_list");
-    const addForm = document.getElementById("add_form");
-
-    try{
-        let characterList = await fetchAllCharacters();
-        const storeCharacters = fetchCharacterFromLocalStorage();
-        characterList = [...characterList, ...storeCharacters];
-
-        console.log("Fetched characters:", characterList)
+const speciesType = document.getElementById("species-type");
+const charactersList = document.getElementById("characters-list");
+const speciesSelect = document.getElementById("species");
 
 
-        // Get all species and add to filter menu
-        const allSpecies = new Set();
-        for(const character of characterList){
-            if(!character.speciesName){
-                character.speciesName = character.species.length > 0
-                    ? await fetchSpecies(character.species)
-                    :"Unknown"
-            }
-            allSpecies.add(character.speciesName);
-        }
-
-        // Update filter-dropdown
-        speciesFilter.innerHTML ='<option value ="all">All</option>';
-        allSpecies.forEach(species => {
-            const option = document.createElement("option");
-            option.value =  species;
-            option.textContent = species;
-            speciesFilter.appendChild(option);
-        });
-
-        // after filter change
-        speciesFilter.addEventListener("change",() =>{
-            const selectedSpecies = speciesFilter.value;
-            const filteredCharacters = selectedSpecies === "all" ? characterList
-            : characterList.filter(char =>char.speciesName === selectedSpecies);
-            renderCharacters(filteredCharacters,allCharacters);
-        });
-
-
-        renderCharacters(characterList, allCharacters);
-        addForm.addEventListener("submit",(e)=>{
-            e.preventDefault();
-            const heroname = document.getElementById("name").value.trim();
-            const yearOfBirth = document.getElementById("birth_year").value;
-            const species = document.getElementById("species").value;
-
-            if (!heroname || !yearOfBirth || !species){
-                alert ("PLease Input all data");
-                return;
-            }
-            if(isNaN(yearOfBirth)){
-                alert("Birth Year must be number");
-                return;
-            }
-            const newCharacter ={
-                name : heroname,
-                birth_year : yearOfBirth,
-                speciesName : species,
-                films :[]
-            };
-
-            storeCharacterToLocalStorage(newCharacter);
-            characterList.push(newCharacter);
-            renderCharacters(characterList,allCharacters);
-
-            addForm.reset();
-        })
-    }catch(error){
-        console.error("Error fetching Start Wars character:", error);
-    }
+async function fetchAllCharacter(){
     
-});
-
-// Fetch all character follow API: https://swapi.dev/ 
-
-async function fetchAllCharacters() {
-    let characterList =[];
-    let nextCharacterUrl = "https://swapi.dev/api/people"; 
-    while(nextCharacterUrl){
-        const characterResponse = await fetch (nextCharacterUrl);
-        const characterData = await characterResponse.json();
-        characterList = characterList.concat(characterData.results);
-        nextCharacterUrl = characterData.next;  // get next page Url
-    }
-    return characterList;
-    
-}
-function storeCharacterToLocalStorage(character){
-    let storeCharacters = JSON.parse(localStorage.getItem("characters")) || [];
-    storeCharacters.push(character);
-    localStorage.setItem("characters", JSON.stringify(storeCharacters));
-}
-
-function fetchCharacterFromLocalStorage(){
-    return JSON.parse(localStorage.getItem("characters")) || [];
-}
-
-
-//Fetch Film Title
-async function fetchFilmTitles(filmUrls) {
-    
-    const filmTitle = await Promise.all(
-        filmUrls.map(async(filmUrl) =>{
-            const filmResponse = await fetch(filmUrl);
-            const filmData = await filmResponse.json();
-            return filmData.title;
-        })
+    let url="https://swapi.dev/api/people/";
+    let characters=[];
+    while(url){
         
-    );
-    return filmTitle;
-    
+        const response =await fetch(url);
+        const data = await response.json();
+        characters=characters.concat(data.results);
+        url=data.next;
+    }
+    return characters;
 }
-async function fetchSpecies(speciesUrls) {
 
-    if(!speciesUrls || !Array.isArray(speciesUrls) || speciesUrls.length === 0){
+async function fetchSpecies(speciesUrl){
+    if(!Array.isArray(speciesUrl) || speciesUrl.length === 0){
         return "Unknown";
     }
-
+    
+    const url = speciesUrl[0];
+    if (typeof url === "string" && !url.startsWith("https://swapi.dev/api/species")){
+        return url;
+    }
     try{
-        const speciesRsponse = await fetch (speciesUrls[0]);
-        if (!speciesRsponse.ok){
-            throw new Error(`Failed : ${speciesUrls[0]}`);
-        }
-        const speciesData = await speciesRsponse.json();
-        return speciesData.name;
-    }catch (error) {
+        const response = await fetch(url);
+        if(!response.ok) throw new Error(`Failed to fetch: ${url}`);
+        const data = await response.json();
+        return data.name || "Unknown";
+    }catch (error){
         console.error("Error fetching species:",error);
         return "Unknown"
     }
-
-    
 }
 
-// background Color based on Species
+async function fetchFilm(filmUrls){
+    const filmstitle = await Promise.all(
+        filmUrls.map(async (url)=>{
+            const response = await fetch (url);
+            const data = await response.json();
+            return data.title;
+        })
+    );
+    return filmstitle;
+}
+
+
 const setSpecicesColor ={
-    "Unknown" : "#ffffff",
-    "Human": "#FF5733",
-    "Wookie":"#3357FF",
-    "Droid":"#33FF57",
-    "Twi'lek": "#2980B9"
+    "Human":"green",
+        "Droid":"purple",
+        "Wookiee":"red",
+        "Rodian":"pink",
+        "Hutt":"blue",
+        "Unknown":"white"
 };
 function getRandomColor(){
     return `#${Math.floor(Math.random()*16777215).toString(16)}`;
 }
-function speciesBackgroundColor(species){
+function getColorForSpecies(species){
     if(!setSpecicesColor[species]){
         setSpecicesColor[species] = getRandomColor();
     }
     return setSpecicesColor[species];
 }
 
-//Render Character Cards
-async function renderCharacters (characterList, container) {
+ async function showCharacters( typesSpecies = null ){
+    try {
+
+    charactersList.innerHTML ="";
+     const apiCharacters = await fetchAllCharacter();
+     const characters=apiCharacters.concat(characterList);
     
-    container.innerHTML ="";
-    for(const character of characterList) {
-        const characterDiv = document.createElement("div");
-        characterDiv.classList.add("character_card");
 
-        //Fetch addition details
-        /* const[speciesName, filmTitle] = await Promise.all([
-            fetchSpecies(character.species),
-            fetchFilmTitles(character.films),
+    for (const character of characters){
+        const speciesName = (character.species && Array.isArray(character.species) && character.species.length > 0)
+        ? await fetchSpecies(character.species)
+        : character.speciesName || "Unknown";
 
-        ]); */
 
-        const speciesName = character.speciesName || "Unknown";
-        const filmTitle =character.films ? await fetchFilmTitles(character.films):["Unknown"];
 
-        //Save species directly in the object for filtering later
-       character.speciesName = speciesName;
+        if (typesSpecies && speciesName !== typesSpecies){
+            continue;
+        }
 
-        // create elements for character card details
-    const characterName = document.createElement("h2");
-    characterName.innerHTML = character.name;
+        const titles = character.films
+        ? await fetchFilm(character.films)
+        : [] ; 
 
-    const characterSpecies = document.createElement("p");
-    characterSpecies.innerHTML =`<strong> Species : <strong> ${speciesName}`;
+        const card = document.createElement("div");
+        card.classList.add("character-card");
+        card.style.backgroundColor = getColorForSpecies(speciesName);
 
-    const characterBirthYear = document.createElement("p");
-    characterBirthYear.innerHTML =`<strong> Birth Year : <strong> ${character.birth_year}`;
+        const characterName  = document.createElement("h2");
+        characterName .innerText =character.name;
 
-    const characterFilms = document.createElement("p");
-    if (filmTitle.length > 0){
-        characterFilms.innerHTML= `<strong> Films : <strong> ${filmTitle.join(",")}`;
-    } else{
-        characterFilms.style.display ="none"
+        const characterBirthYear = document.createElement("p");
+        characterBirthYear.innerText ="BirthYear:" + character.birth_year;
+
+        const characterSpecies = document.createElement("p");
+        characterSpecies.innerText ="Species:" + speciesName;
+
+        const characterFilms = document.createElement("p");
+        characterFilms.innerText = titles.length > 0
+            ? "Filmer:" + titles.join(",")
+            : "Filmer : Unknown";
+
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML=`<i class="fa-solid fa-trash"></i>`;
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.addEventListener("click", function(){
+            card.remove();
+
+    
+        });
+
+
+        const editBtn = document.createElement("button");
+        editBtn.innerHTML=`<i class="fa-solid fa-pen"></i>`;
+        editBtn.classList.add("edit-btn");
+        editBtn.addEventListener("click",function(){
+            const newName = prompt("Enter New Name: ", character.name);
+            const newBirthYear = prompt("Enter New Birth Year: ", character.birth_year);
+            const newSpecies = prompt("Enter New Specie: ", speciesName);
+
+            if (newName)characterName.innerText = newName;
+            if(newBirthYear)characterBirthYear.innerText="BirthYear: " + newBirthYear;
+            if (newSpecies){
+                characterSpecies.innerText ="Species: " + newSpecies;
+                card.style.backgroundColor = getColorForSpecies(newSpecies)
+            }
+
+        });
+
+        card.append(characterName, characterBirthYear, characterSpecies,characterFilms,deleteBtn,editBtn);
+        charactersList.appendChild(card);
     }
-
-
-    // create edit button
-    const editButton = document.createElement("button");
-    editButton.innerHTML = `<i class="fa-solid fa-pen"></i>`;
-    editButton.classList.add("edit-btn");
-    editButton.addEventListener("click", () =>{
-        const newName = prompt("Enter new name: ", character.name);
-        const newBirthYear = prompt("Enter new birth year: ", character.birth_year);
-        const newSpecies = prompt("Enter new species: ", speciesName);
-        if (newName) characterName.innerHTML = newName;
-        if(newBirthYear) characterBirthYear.innerHTML = `<strong> Birth Year : <strong> ${newBirthYear}`;
-        if(newSpecies) characterSpecies.innerHTML = `<strong> Species : <strong> ${newSpecies}`;
-        characterDiv.style.backgroundColor = speciesBackgroundColor(newSpecies);
-    });
-
-
-    //Create delete button
-
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-    deleteButton.classList.add("delete-btn");
-    deleteButton.addEventListener("click",() => {
-        characterDiv.remove();
-    });
-
-    // background Color
-    characterDiv.style.backgroundColor = speciesBackgroundColor(speciesName);
-
-
-    characterDiv.appendChild(characterName);
-    characterDiv.appendChild(characterSpecies);
-    characterDiv.appendChild(characterBirthYear);
-    characterDiv.appendChild(characterFilms);
-    container.appendChild(characterDiv);
-    characterDiv.appendChild(editButton);
-    characterDiv.appendChild(deleteButton);
-
-
+    console.log(" Character Cards : ", charactersList.children.length);
+        
+    } catch (error) {
+        console.log("Can not loaded characet", error)
     }
-
     
 }
+
+async function addSpeciesBtn(){
+    const characters = await fetchAllCharacter();
+    const speciesGroup = new Set();
+
+    for (const character of characters){
+        const speciesName =  (character.species && character.species.length > 0)
+        ? await fetchSpecies (character.species)
+        :"Unknown";
+        speciesGroup.add(speciesName);
+    }
+
+    speciesGroup.forEach(species => {
+        const buttonBtn = document.createElement("button");
+        buttonBtn.innerText = species;
+        buttonBtn.addEventListener("click", function(){
+            showCharacters(species);
+        });
+
+        speciesType.appendChild(buttonBtn);
+
+        // dropdown
+        const option =document.createElement("option");
+        option.value =species;
+        option.innerText=species;
+        speciesSelect.appendChild(option);
+    });
+
+    const restartBtn = document.createElement("button");
+    restartBtn.innerText = "All";
+    restartBtn.addEventListener("click", function(){
+        showCharacters();
+    });
+    speciesType.appendChild(restartBtn);
+
+}
+
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click",createCharacter);
+const characterList = []; 
+function createCharacter(){
+    try{
+        //const allCharacters = document.getElementById("all_character");
+        const inputCharacterName = document.getElementById("name").value.trim();
+        const inputYearOfBirth = document.getElementById("birth_year").value;
+        const inputspecies = speciesSelect.value;
+        if (!inputCharacterName || !inputYearOfBirth || !inputspecies || inputspecies === "all"){
+            alert ("PLease Input all data");
+            return;
+        }   
+        const newCharacterObject  = {
+            name : inputCharacterName,
+            birth_year : inputYearOfBirth,
+            speciesName : [inputspecies]
+        };
+        console.log("New character:", newCharacterObject);
+        characterList.push(newCharacterObject);
+       
+        localStorage.setItem("characters",JSON.stringify(characterList));
+        showCharacters();
+        
+
+
+    
+  
+    }catch(error)
+        {
+            console.log("Can not create new character:", error);
+        }
+        
+}
+
+addSpeciesBtn();
+showCharacters();
+
+
+
+
+
+
+
